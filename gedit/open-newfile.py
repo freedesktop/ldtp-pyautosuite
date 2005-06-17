6569130fa1adc7e6c7a33a3b5b4547a03a5d57b7
sshaik <sshaik>
2005-06-17 13:59:39 +0000

    Removed old scripts

--- gedit/open-newfile.py
+++
@@ -1,7 +0,0 @@
-#To open a new file
-log ('Open-New-File', 'teststart')
-selectmenuitem ('gedit', 'mnuFile;mnuClose')
-selectmenuitem ('gedit', 'mnuFile;mnuNew')
-selectmenuitem ('gedit', 'mnuFile;mnuClose')
-log ('Pass', 'pass')
-log ('Open-New-File', 'testend')
--- gedit/pagesetup.py
+++
@@ -1,8 +0,0 @@
-#To open pagesetup dialog
-log ('Launch-PageSetup', 'teststart')
-selectmenuitem ('gedit', 'mnuFile;mnuPageSetup');
-click ('dlgPageSetup', 'btnClose')
-selectmenuitem ('gedit', 'mnuFile;mnuCloseAll')
-selectmenuitem ('gedit', 'mnuFile;mnuQuit')
-log ('Launched Page Setup dialog successfully','pass')
-log ('Launch-PageSetup','testend')
